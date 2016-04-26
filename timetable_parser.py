ce4194ad7cc45f89bf1c3d98bd5663c5d6ec33c2
Giannie <ggrasso@blueyonder.co.uk>
2016-04-26 07:55:19 +0100

    Update timetable_parser.py

--- timetable_parser.py
+++ timetable_parser.py
@@ -1,7 +1,7 @@
 from datetime import time, datetime
 import xml.etree.ElementTree as et
 
-xml_file = "/Volumes/Home Drive/Users/giancarlo/Downloads/PAD.xml"
+xml_file = "/path/to/file.xml"
 
 days = ["Mon", "Tue", "Wed", "Thu", "Fri"]
 weeks = ["A", "B"]
@@ -16,14 +16,27 @@ def LessonDict(group="", room=""):
     return {"group": group, "room": room}
 
 def PeriodDict(title, start, end):
-        self = {"title": title, "start": start, "end": end}
+    return {"title": title, "start": start, "end": end}
 
 class TimeTableClass(list):
     def __init__(self, xml_file):
         super(TimeTableClass, self).__init__()
         tree = et.parse(xml_file)
         root = tree.getroot()
-
+        
+        day_struct = [PeriodDict("SRG", time(8, 30), time(8, 40)),
+              PeriodDict("S1", time(8, 45), time(9, 35)),
+              PeriodDict("S2", time(9, 40), time(10, 30)),
+              PeriodDict("SB1", time(10, 30), time(10, 50)),
+              PeriodDict("SB2", time(10, 50), time(11, 10)),
+              PeriodDict("S3", time(11, 15), time(12, 5)),
+              PeriodDict("S4", time(12, 10), time(13, 0)),
+              PeriodDict("SL1", time(13, 0), time(13, 25)),
+              PeriodDict("SL2", time(13, 25), time(14, 5)),
+              PeriodDict("S5", time(14, 15), time(15, 5)),
+              PeriodDict("S6", time(15, 10), time(16, 0)),
+              PeriodDict("SED", time(16, 10), time(17, 0))]
+        
         for item in root[0][1][0][4:16]:
             row = []
             for lesson_xml in item[1:]:
@@ -36,17 +49,3 @@ class TimeTableClass(list):
     def column(self, number):
         c = [self[i][0] for i in range(len(self))]
         return c
-
-
-day_struct = [PeriodDict("SRG", time(8, 30), time(8, 40)),
-              PeriodDict("S1", time(8, 45), time(9, 35)),
-              PeriodDict("S2", time(9, 40), time(10, 30)),
-              PeriodDict("SB1", time(10, 30), time(10, 50)),
-              PeriodDict("SB2", time(10, 50), time(11, 10)),
-              PeriodDict("S3", time(11, 15), time(12, 5)),
-              PeriodDict("S4", time(12, 10), time(13, 0)),
-              PeriodDict("SL1", time(13, 0), time(13, 25)),
-              PeriodDict("SL2", time(13, 25), time(14, 5)),
-              PeriodDict("S5", time(14, 15), time(15, 5)),
-              PeriodDict("S6", time(15, 10), time(16, 0)),
-              PeriodDict("SED", time(16, 10), time(17, 0))]
