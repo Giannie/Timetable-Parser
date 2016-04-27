3ceea4952ef0636798eff4102cfb56c6bba0f217
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2016-04-27 19:32:48 +0100

    Add calendar generation button

--- timetable_gui.py
+++ timetable_gui.py
@@ -11,12 +11,22 @@ class TimetableApp(QtGui.QMainWindow, qt_layout.Ui_MainWindow):
             date_edit.setDate(QtCore.QDate.currentDate())
         self.xml_choice_button.clicked.connect(self.selectXMLFile)
         self.target_choice_button.clicked.connect(self.selectTarget)
+        self.genCalButton.clicked.connect(self.generateCalendars)
 
     def selectXMLFile(self):
         self.xml_line_edit.setText(QtGui.QFileDialog.getOpenFileName())
 
     def selectTarget(self):
         self.target_line_edit.setText(QtGui.QFileDialog.getExistingDirectory())
+
+    def generateCalendars(self):
+        calGroup = tp.TimeTableGroup(self.xml_line_edit.text)
+        target = self.target_line_edit.text
+        term_start = self.term_start_edit.date.toPyDate()
+        half_end = self.half_end_edit.date.toPyDate()
+        half_start = self.half_start_edit.date.toPyDate()
+        term_end = self.term_end_edit.date.toPyDate()
+        calGroup.generate_calendars(term_start, half_end, half_start, term_end, path=target)
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
