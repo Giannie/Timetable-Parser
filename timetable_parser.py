f02620a70ef6e95ac682c953b1ae3754837008b8
Giannie <ggrasso@blueyonder.co.uk>
2016-04-27 11:47:37 +0100

    Strip timetable from name

--- timetable_parser.py
+++ timetable_parser.py
@@ -121,7 +121,7 @@ class TimeTableGroup(dict):
         timetables_tag = root[0][1]
         for xml_tag in timetables_tag:
             timetable = TimeTableClass(xml_tag)
-            name = xml_tag[1].text
+            name = xml_tag[1].text.strip('Timetable - ')
             self[name] = timetable
     
     def generate_calendars(self, term_start, half_end, half_start, term_end, path=''):
