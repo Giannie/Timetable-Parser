e3a23be9bf2c61d68970c1f5bcb9e811a64696f7
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2016-05-02 19:47:14 +0100

    Test commit

---
+++ timetable_gui5.py
@@ -0,0 +1,111 @@
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtWidgets import QApplication, QWidget
+import qt_layout5
+import timetable_parser as tp
+import sys
+import datetime
+import threading
+import os
+import configparser
+import qdarkstyle
+
+with open("style.qss", 'r') as f:
+    style = f.read()
+
+class TimetableApp(QWidget, qt_layout5.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(TimetableApp, self).__init__(parent)
+        self.setupUi(self)
+        self.date_edit_list = [self.term_start_edit, self.half_end_edit, self.half_start_edit, self.term_end_edit]
+        for index in range(len(self.date_edit_list)):
+            date_edit = self.date_edit_list[index]
+            date_edit.setDate(QtCore.QDate.currentDate())
+            date_edit.calendarWidget().setFirstDayOfWeek(1)
+            date_edit.dateChanged.connect(self.lambdaGen(index))
+            if index > 0:
+                date_edit.setMinimumDate(QtCore.QDate.currentDate())
+
+        self.xml_choice_button.clicked.connect(self.selectXMLFile)
+        self.target_choice_button.clicked.connect(self.selectTarget)
+        self.genCalButton.clicked.connect(self.generateCalendars)
+        self.dateFileButton.clicked.connect(self.selectDateFile)
+        self.quitButton.clicked.connect(self.close)
+        self.first_run = True
+
+    def updateDates(self, index=0):
+        if index < 3:
+            self.date_edit_list[index + 1].setMinimumDate(self.date_edit_list[index].date())
+
+    def lambdaGen(self, i):
+        return lambda: self.updateDates(i)
+
+    def selectXMLFile(self):
+        self.xml_line_edit.setText(QtGui.QFileDialog.getOpenFileName())
+
+    def selectTarget(self):
+        self.target_line_edit.setText(QtGui.QFileDialog.getExistingDirectory())
+
+    def selectDateFile(self):
+        self.selectDates(QtGui.QFileDialog.getOpenFileName())
+
+    def selectDates(self, filename):
+        if filename:
+            config = configparser.ConfigParser()
+            config.read(filename)
+            choices = config.sections()
+            term, ok = QtGui.QInputDialog.getItem(self, "Select Term", "Select term: ", choices, 0, False)
+            if ok:
+                term_choice = config[term]
+                self.term_start_edit.setDate(QtCore.QDate.fromString(term_choice["term_start"], "dd'/'MM'/'yyyy"))
+                self.half_end_edit.setDate(QtCore.QDate.fromString(term_choice["half_end"], "dd'/'MM'/'yyyy"))
+                self.half_start_edit.setDate(QtCore.QDate.fromString(term_choice["half_start"], "dd'/'MM'/'yyyy"))
+                self.term_end_edit.setDate(QtCore.QDate.fromString(term_choice["term_end"], "dd'/'MM'/'yyyy"))
+
+
+    def generateCalendars(self):
+        target = self.target_line_edit.text()
+        term_start = datetime.datetime.combine(self.term_start_edit.date().toPyDate(), datetime.time())
+        half_end = datetime.datetime.combine(self.half_end_edit.date().toPyDate(), datetime.time())
+        half_start = datetime.datetime.combine(self.half_start_edit.date().toPyDate(), datetime.time())
+        term_end = datetime.datetime.combine(self.term_end_edit.date().toPyDate(), datetime.time())
+        if not(os.path.isfile(target)):
+            self.workLabel.setText("XML file not found")
+        elif not(os.path.isdir(target)):
+            self.workLabel.setText("Target directory not found")
+        else:
+            self.workLabel.setText('Working...')
+            dates = tp.timetableDates(term_start=term_start, half_end=half_end, half_start=half_start, term_end=term_end)
+            calGroup = tp.TimeTableGroup(self.xml_line_edit.text(), dates)
+            if not(self.first_run) and self.calThread.running:
+                pass
+            else:
+                self.first_run = False
+                self.calThread = calendarThread(calGroup, target, parent=self)
+                self.calThread.start()
+
+
+class calendarThread(threading.Thread):
+    def __init__(self, calendarGroup, target, parent=None):
+        threading.Thread.__init__(self)
+        self.calendarGroup = calendarGroup
+        self.target = target
+        self.parent = parent
+        self.running = True
+
+    def run(self):
+        try:
+            self.calendarGroup.generate_calendars(self.target)
+            self.parent.workLabel.setText('Done')
+            self.running = False
+        except:
+            self.parent.workLabel.setText('Error')
+            self.running = False
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyleSheet(style)
+#    app.setStyleSheet(qdarkstyle.load_stylesheet(pyside=False))
+    form = TimetableApp()
+    form.show()
+    app.exec_()
